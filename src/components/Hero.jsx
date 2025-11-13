f9b45624ed920dc07e9d87340ce9cbda1295fe78
Aswin <itsme.aswinkp@gmail.com>
2025-11-13 15:30:14 +0530

    feat(hero): add hero with realistic video component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useRef} from 'react'
+
+const Hero = () => {
+
+    //for more control over the hero-video
+    const videoRef = useRef(null);
+
+    //for do crazy actions
+    useEffect(() => {
+        if(videoRef.current) videoRef.current.playbackRate = 2; //for control speed!
+    }, []);
+
+    return (
+        <section id={'hero'}>
+            <div>
+                <h1>MacBook Pro</h1>
+                <img src="/title.png" alt="Macbook Title"/>
+            </div>
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+            <button>Buy</button>
+            <p>From $1599 or $133/mo for 12 months</p>
+        </section>
+    )
+}
+export default Hero
