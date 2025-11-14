47c1ab4aac362b5596224f2aaec2ed5b4544b357
Aswin <itsme.aswinkp@gmail.com>
2025-11-14 21:14:53 +0530

    feat(product-viewer): ModelSwitcher completed with GSAP animations !

--- src/components/three/ModelSwitcher.jsx
+++ src/components/three/ModelSwitcher.jsx
@@ -2,6 +2,28 @@ import React, {useRef} from 'react'
 import {PresentationControls} from "@react-three/drei";
 import MacbookModel16 from "../models/Macbook-16.jsx";
 import MacbookModel14 from "../models/Macbook-14.jsx";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+const ANIMATION_DURATION = 1;
+const OFFSET_DISTANCE = 5;
+
+// making utility function always better for standardizing codebase !! // ingane indakkiyal nalla control kittum.
+const fadeMeshes = (group, opacity) =>{
+    if(!group) return;
+    group.traverse((child)=>{
+        if(child.isMesh){
+            child.material.transparent = true;
+            gsap.to(child.material, { opacity, duration: ANIMATION_DURATION });
+        }
+    })
+}
+const moveGroups = (group, x) =>{
+    if(!group) return;
+    gsap.to(group.position, { x, duration: ANIMATION_DURATION });
+}
+
+
 
 const ModelSwitcher = ({ scale , isMobile }) => {
 
@@ -9,6 +31,23 @@ const ModelSwitcher = ({ scale , isMobile }) => {
     const largeMacbookRef = useRef(null);
 
     const showLargeMacbook = scale === 0.08 || scale === 0.05;
+
+    useGSAP(()=>{
+        if(showLargeMacbook){
+            moveGroups(smallMacbookRef.current, -OFFSET_DISTANCE);
+            moveGroups(largeMacbookRef.current, 0);
+
+            fadeMeshes(smallMacbookRef.current, 0);
+            fadeMeshes(largeMacbookRef.current, 1);
+        }else{
+            moveGroups(smallMacbookRef.current, 0);
+            moveGroups(largeMacbookRef.current, OFFSET_DISTANCE);
+
+            fadeMeshes(smallMacbookRef.current, 1);
+            fadeMeshes(largeMacbookRef.current, 0);
+
+        }
+    },[scale])
 
     const controlsConfig = {
         // global: false, // Spin globally or by dragging the model
@@ -31,11 +70,11 @@ const ModelSwitcher = ({ scale , isMobile }) => {
                     <MacbookModel16 scale={isMobile ? 0.05 : 0.08}/>
                 </group>
             </PresentationControls>
-            {/*<PresentationControls {...controlsConfig}>*/}
-            {/*    <group ref={largeMacbookRef}>*/}
-            {/*        <MacbookModel14 scale={isMobile ? 0.03 : 0.06}/>*/}
-            {/*    </group>*/}
-            {/*</PresentationControls>*/}
+            <PresentationControls {...controlsConfig}>
+                <group ref={smallMacbookRef}>
+                    <MacbookModel14 scale={isMobile ? 0.03 : 0.06}/>
+                </group>
+            </PresentationControls>
         </>
     )
 }
